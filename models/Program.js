2e4cce3cc49f2ed068bb6107290c0b0f43905579
Naveen Kumar P <naveenkumarp2094@gmail.com>
2026-02-02 19:44:35 +0530

    Update Program.js

--- models/Program.js
+++ models/Program.js
@@ -1,10 +1,22 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
-const ProgramSchema = new mongoose.Schema({
-  title: String,
-  shortDesc: String,
-  longDesc: String,
-  image: String,
-});
+const ProgramSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    shortDesc: { type: String, required: true },
+    longDesc: { type: String, required: true },
+    image: { type: String },
+    status: {
+      type: String,
+      enum: ["pending", "verified", "closed"],
+      default: "pending",
+    },
+    createdBy: {
+      type: String, // user name or user id
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Program', ProgramSchema);
+export default mongoose.model("Program", ProgramSchema);
